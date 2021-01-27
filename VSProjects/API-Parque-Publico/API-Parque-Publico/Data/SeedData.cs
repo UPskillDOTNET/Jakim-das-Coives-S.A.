1ba9f415def3b945a10b42581e90b44043d68b51
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-01-27 17:48:30 +0000

    SeedData corrigido

--- VSProjects/API-Parque-Publico/API-Parque-Publico/Data/SeedData.cs
+++ VSProjects/API-Parque-Publico/API-Parque-Publico/Data/SeedData.cs
@@ -19,23 +19,26 @@ namespace API_Parque_Publico.Data
             context.Clientes.AddRange(
                 new Cliente { Nif = 111222333 , Nome = "Jakim", Email = "jakim@upskill.pt" }
             );
-            
+            context.SaveChanges();
+
             context.Freguesias.AddRange(
                 new Freguesia { Nome = "Porto" }
             );
-            
+            context.SaveChanges();
+
             context.Parques.AddRange(
                 new Parque { Rua = "D. Joao" , FreguesiaId = 1 }
             );
-            
+            context.SaveChanges();
+
             context.Lugares.AddRange(
                 new Lugar { Numero = 1, Fila = "A" , Andar = 0, ParqueId = 1 , Preco = 5.99 }
             );
+            context.SaveChanges();
 
             context.Reservas.AddRange(
                 new Reserva { NifCliente = 111222333, LugarId = 1, Inicio = DateTime.Parse("2021-01-30 11:00:00") , Fim = DateTime.Parse("2021-01-30 12:00:00") }
             );
-
             context.SaveChanges();
         }
     }
