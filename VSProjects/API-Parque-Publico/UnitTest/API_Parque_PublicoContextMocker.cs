e6875463e8489a551a47f0ddf8787a602faada9c
Joao Vilaverde <nuno-maister@upskill.pt>
2021-01-27 17:31:18 +0000

    Populated seed and created empty controller test

--- VSProjects/API-Parque-Publico/UnitTest/API_Parque_PublicoContextMocker.cs
+++ VSProjects/API-Parque-Publico/UnitTest/API_Parque_PublicoContextMocker.cs
@@ -30,6 +30,69 @@ namespace UnitTest
                 Nome = "Jakim das Coives S.A.",
                 Email = "help@jakimdascoives.pt"
             });
+            dbContext.Clientes.Add(new Cliente
+            {
+                Nif = 987654321,
+                Nome = "Jakim",
+                Email = "help@jakimdascoives.pt"
+            });
+            dbContext.Freguesias.Add(new Freguesia
+            {
+                Id = 1,
+                Nome = "Jakim das Coives",
+            });
+            dbContext.Freguesias.Add(new Freguesia
+            {
+                Id = 2,
+                Nome = "Jakim",
+            });
+            dbContext.Parques.Add(new Parque
+            {
+                Id = 1,
+                Rua = "sao bento",
+                FreguesiaId = 1
+            });
+            dbContext.Parques.Add(new Parque
+            {
+                Id = 2,
+                Rua = "Campanha",
+                FreguesiaId = 1
+            });
+            dbContext.Lugares.Add(new Lugar
+            {
+                Id = 1,
+                ParqueId = 1,
+                Numero = 1,
+                Fila = "1",
+                Andar = 1,
+                Preco = 1
+            });
+            dbContext.Lugares.Add(new Lugar
+            {
+                Id = 2,
+                ParqueId = 1,
+                Numero = 1,
+                Fila = "2",
+                Andar = 1,
+                Preco = 1
+            });
+            dbContext.Reservas.Add(new Reserva
+            {
+                Id = 1,
+                NifCliente = 123456789,
+                LugarId = 1,
+                Inicio = DateTime.Parse("2021-01-01 10:10:10"),
+                Fim = DateTime.Parse("2021-01-02 10:10:10")
+            });
+            dbContext.Reservas.Add(new Reserva
+            {
+                Id = 2,
+                NifCliente = 987654321,
+                LugarId = 1,
+                Inicio = DateTime.Parse("2021-01-01 10:10:10"),
+                Fim = DateTime.Parse("2021-01-02 10:10:10")
+            });
+
         }
     }
 }
