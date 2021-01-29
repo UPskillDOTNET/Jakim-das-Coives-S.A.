9f456c4e697564615f48c3ddc0a2c3f25d32095a
Joao Vilaverde <nuno-maister@upskill.pt>
2021-01-29 10:16:46 +0000

    created seed Parque privado 2

---
+++ VSProjects/API-Parque-Privado2/API-Parque-Privado2/Data/SeedData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Parque_Privado2.Data
+{
+    public class SeedData
+    {
+    }
+}
