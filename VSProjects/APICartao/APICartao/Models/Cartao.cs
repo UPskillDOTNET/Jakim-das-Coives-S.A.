01467a35e99cf829b8c5ed00c32fff19a4b1552f
Daniel Macedo <dcmacedo02@upskill.pt>
2021-02-04 12:30:16 +0000

    Criação do Controller e das Migrations API CC

---
+++ VSProjects/APICartao/APICartao/Models/Cartao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICartao.Models
+{
+    public class Cartao
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Numero { get; set; }
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime DataValidade { get; set; }
+
+        [Required]
+        public int Cvc { get; set; }
+
+        [Required]
+        public double Custo { get; set; }
+        
+        [Required]
+        public int NifDestinatario { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime Data { get; set; }
+
+    }
+}
--- VSProjects/APICartao/APICartao/Startup.cs
+++ VSProjects/APICartao/APICartao/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APICartao.Data;
 
 namespace APICartao
 {
@@ -32,6 +34,9 @@ namespace APICartao
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APICartao", Version = "v1" });
             });
+
+            services.AddDbContext<APICartaoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("APICartaoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
