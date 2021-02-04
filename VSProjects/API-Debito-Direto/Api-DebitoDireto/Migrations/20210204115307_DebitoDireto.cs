b7c008574287dcab099ba18afa3b3cf7e0c52745
Jose Franco <jpmfranco@upskill.pt>
2021-02-04 15:19:49 +0000

    Debito Direto API

---
+++ VSProjects/API-Debito-Direto/Api-DebitoDireto/Migrations/20210204115307_DebitoDireto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api_DebitoDireto.Migrations
+{
+    public partial class DebitoDireto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DebitoDireto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    iban = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    rua = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    codigoPostal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    freguesia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    nifDestinatario = table.Column<int>(type: "int", nullable: false),
+                    custo = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DebitoDireto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DebitoDireto");
+        }
+    }
+}
---
+++ VSProjects/API-Debito-Direto/Api-DebitoDireto/Models/DebitoDireto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api_DebitoDireto.Models
+{
+    public class DebitoDireto
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string iban { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string rua { get; set; }
+        [Required]
+        public string codigoPostal { get; set; }
+        [Required]
+        public string freguesia { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime data { get; set; }
+        [Required]
+        public int nifDestinatario { get; set; }
+        [Required]
+        public double custo { get; set; }
+
+
+    }
+}
