faf2df56b453fa4e5a8223639217d197db7d42e5
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-02-05 13:58:22 +0000

    Token Service para gerar JWT tokens

--- VSProjects/API-Sistema-Central/API-Sistema-Central/Controllers/UtilizadoresController.cs
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Controllers/UtilizadoresController.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using API_Sistema_Central.DTOs;
 using API_Sistema_Central.Models;
-using Microsoft.AspNetCore.Http;
+using API_Sistema_Central.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace API_Sistema_Central.Controllers
 {
@@ -21,25 +13,24 @@ namespace API_Sistema_Central.Controllers
     {
         private readonly UserManager<Utilizador> _userManager;
         private readonly SignInManager<Utilizador> _signInManager;
-        private readonly IConfiguration _configuration;
-        public UtilizadoresController(UserManager<Utilizador> userManager,
-            SignInManager<Utilizador> signInManager,
-            IConfiguration configuration)
+        private readonly ITokenService _tokenService;
+        public UtilizadoresController(UserManager<Utilizador> userManager, SignInManager<Utilizador> signInManager, ITokenService tokenService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _configuration = configuration;
+            _tokenService = tokenService;
         }
 
         [HttpPost("Registar")]
         public async Task<ActionResult<TokenUtilizadorDTO>> RegistarUtilizador([FromBody] RegistarUtilizadorDTO model)
         {
-            var info = new InfoUtilizadorDTO { Email = model.Email, Password = model.Password };
+            var infoUtilizadorDTO = new InfoUtilizadorDTO { Email = model.Email, Password = model.Password };
             var user = new Utilizador { Id = model.Nif, UserName = model.Nome, Nome = model.Nome, Email = model.Email, CredencialId = model.CredencialId };
             var result = await _userManager.CreateAsync(user, model.Password);
+            TokenUtilizadorDTO token = _tokenService.BuildToken(infoUtilizadorDTO);
             if (result.Succeeded)
             {
-                return BuildToken(info);
+                return  token;
             }
             else
             {
@@ -48,45 +39,21 @@ namespace API_Sistema_Central.Controllers
         }
 
         [HttpPost("Login")]
-        public async Task<ActionResult<TokenUtilizadorDTO>> Login([FromBody] InfoUtilizadorDTO InfoUtilizadorDTO)
+        public async Task<ActionResult<TokenUtilizadorDTO>> Login([FromBody] InfoUtilizadorDTO infoUtilizadorDTO)
         {
-            var result = await _signInManager.PasswordSignInAsync(InfoUtilizadorDTO.Email, InfoUtilizadorDTO.Password,
+            var result = await _signInManager.PasswordSignInAsync(infoUtilizadorDTO.Email, infoUtilizadorDTO.Password,
                  isPersistent: false, lockoutOnFailure: false);
+            TokenUtilizadorDTO token = _tokenService.BuildToken(infoUtilizadorDTO);
 
             if (result.Succeeded)
             {
-                return BuildToken(InfoUtilizadorDTO);
+                return token;
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "login inválido.");
                 return BadRequest(ModelState);
             }
-        }
-
-        private TokenUtilizadorDTO BuildToken(InfoUtilizadorDTO InfoUtilizadorDTO)
-        {
-            var claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.UniqueName, InfoUtilizadorDTO.Email),
-                new Claim("meuValor", "oque voce quiser"),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            // tempo de expiração do token: 1 hora
-            var expiration = DateTime.UtcNow.AddHours(1);
-            JwtSecurityToken token = new JwtSecurityToken(
-               issuer: null,
-               audience: null,
-               claims: claims,
-               expires: expiration,
-               signingCredentials: creds);
-            return new TokenUtilizadorDTO()
-            {
-                Token = new JwtSecurityTokenHandler().WriteToken(token),
-                Expiration = expiration
-            };
         }
     }
 }
---
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using API_Sistema_Central.DTOs;
+
+namespace API_Sistema_Central.Services
+{
+    public interface ITokenService
+    {
+        public TokenUtilizadorDTO BuildToken(InfoUtilizadorDTO InfoUtilizadorDTO);
+    }
+}
