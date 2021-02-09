49d83d07ac04da5bf4071b8864678e5052bcee40
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-02-09 09:49:32 +0000

    Reserva Service v0.2

---
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Services/ReservaService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Sistema_Central.Models;
+using API_Sistema_Central.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Sistema_Central.Services
+{
+    public class ReservaService
+    {
+        private readonly IReservaRepository _repository;
+
+        public ReservaService(IReservaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ActionResult<IEnumerable<Reserva>>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Reserva> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task PutAsync(Reserva Reserva)
+        {
+            await _repository.PutAsync(Reserva);
+        }
+
+        public async Task<Reserva> PostAsync(Reserva Reserva)
+        {
+            return await _repository.PostAsync(Reserva);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
