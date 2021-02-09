6c8e38050f2604cb7c3030673a3a48d3fc8900b7
Daniel Macedo <dcmacedo02@upskill.pt>
2021-02-09 17:01:33 +0000

    Criação PagamentoService API SC

---
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Services/PagamentoService.cs
@@ -0,0 +1,32 @@
+﻿using API_Sistema_Central.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Sistema_Central.Services
+{
+    public class PagamentoService : IPagamentoService
+    {
+        Task<ActionResult> IPagamentoService.PayWithCartao(CartaoDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ActionResult> IPagamentoService.PayWithCarteira(string nif)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ActionResult> IPagamentoService.PayWithDebitoDireto(DebitoDiretoDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ActionResult> IPagamentoService.PayWithPayPal(PayPalDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
