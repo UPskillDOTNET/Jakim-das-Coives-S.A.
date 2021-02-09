d1b20d0fc7638010b66453791a6ec5791caee715
Ines Oliveira <ines.machado.oliveira@upskill.pt>
2021-02-09 10:29:33 +0000

    Changed Controllers and Seed Data API Paypal

--- VSProjects/API-Paypal/API-Paypal/Startup.cs
+++ VSProjects/API-Paypal/API-Paypal/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using API_Paypal.Data;
+using API_Paypal.Repositories;
+using API_Paypal.Services;
 
 namespace API_Paypal
 {
@@ -30,13 +32,16 @@ namespace API_Paypal
         {
 
             services.AddControllers();
-            /*services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_Paypal", Version = "v1" });
-            });*/
+            });
 
             services.AddDbContext<API_PaypalContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("API_PaypalContext")));
+
+            services.AddTransient<IPaypalService, PaypalService>();
+            services.AddScoped<IPaypalRepository, PaypalRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +50,8 @@ namespace API_Paypal
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                /*app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_Paypal v1"));*/
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_Paypal v1"));
             }
 
             app.UseHttpsRedirection();
