5e2cf317c6d0051f9f4325f423be085cedb9698e
Daniel Macedo <dcmacedo02@upskill.pt>
2021-02-09 15:58:33 +0000

    Interface Serviço Pagamento Sistema Central

---
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Services/IPagamentoService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Sistema_Central.Services
+{
+    public interface IPagamentoService
+    {
+        public Task<ActionResult> PayWithCarteira(string nif);
+        public Task<ActionResult> PayWithCard();
+        public Task<ActionResult> PayWithPayPal();
+        public Task<ActionResult> PayWithDebitoDireto();
+    }
+}
