089379baeb4a338fe5823a87e7660035f263210b
Daniel Macedo <dcmacedo02@upskill.pt>
2021-02-09 17:48:05 +0000

    Desenvolvimento de Serviço Pagamento Com Cartao

--- VSProjects/API-Sistema-Central/API-Sistema-Central/Services/PagamentoService.cs
+++ VSProjects/API-Sistema-Central/API-Sistema-Central/Services/PagamentoService.cs
@@ -1,16 +1,47 @@
 ﻿using API_Sistema_Central.DTOs;
+using API_Sistema_Central.Models;
+using API_Sistema_Central.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace API_Sistema_Central.Services
 {
     public class PagamentoService : IPagamentoService
     {
-        Task<ActionResult> IPagamentoService.PayWithCartao(CartaoDTO dTO)
+        private readonly IMetodoPagamentoRepository _repository;
+        private HttpClient _client;
+
+        public PagamentoService(IMetodoPagamentoRepository repository)
         {
+            _repository = repository;
+            _client = new HttpClient();
+        }
+
+        async Task<ActionResult> IPagamentoService.PayWithCartao(CartaoDTO dTO)
+        {
+            MetodoPagamento cartaoMetodo = await _repository.GetByIdAsync(1);
+            _client.BaseAddress = new Uri(cartaoMetodo.ApiUrl);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var cartaoURI = "Cartoes";
+
+            try
+            {
+                var response = _client.PostAsJsonAsync(cartaoURI, dTO);
+
+            } catch
+            {
+                throw;
+            }
+
             throw new NotImplementedException();
         }
 
