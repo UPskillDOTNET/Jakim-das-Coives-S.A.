edc1a8925d36580023d66e69fd5687b376ac7b7a
Joao Vilaverde <nuno-maister@upskill.pt>
2021-02-09 11:25:37 +0000

    Created repository in API-SubAluguer

---
+++ VSProjects/API-SubAluguer/API-SubAluguer/Repository/ILugarRepository.cs
@@ -0,0 +1,12 @@
+﻿using API_SubAluguer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SubAluguer.Repository
+{
+    public interface ILugarRepository : IRepositoryBase<Lugar>
+    {
+    }
+}
