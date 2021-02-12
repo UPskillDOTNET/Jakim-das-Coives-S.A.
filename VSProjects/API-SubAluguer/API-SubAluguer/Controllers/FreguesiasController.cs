5e4e32497ae6148d8967a5304d89db1b7a126c90
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-02-12 20:21:35 +0000

    Disponibilidade em todos os parques Nice and Funfing

---
+++ VSProjects/API-SubAluguer/API-SubAluguer/Controllers/FreguesiasController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_SubAluguer.Models;
+using API_SubAluguer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_SubAluguer.Controllers
+{
+    [Route("api/freguesias")]
+    [ApiController]
+    public class FreguesiasController : ControllerBase
+    {
+        private readonly IFreguesiaService _service;
+
+        public FreguesiasController(IFreguesiaService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Freguesia>>> GetAllLugar()
+        {
+            return await _service.GetAllAsync();
+        }
+    }
+}
