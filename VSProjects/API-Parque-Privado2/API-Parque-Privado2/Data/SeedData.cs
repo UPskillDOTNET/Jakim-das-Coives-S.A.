6627c79ae1781379bfe349026b9861a9eec2fa02
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-02-12 15:04:35 +0000

    SeedData dos Parques Sincronizados

--- VSProjects/API-Parque-Privado2/API-Parque-Privado2/Data/SeedData.cs
+++ VSProjects/API-Parque-Privado2/API-Parque-Privado2/Data/SeedData.cs
@@ -16,43 +16,39 @@ namespace API_Parque_Privado2.Data
                 return;   // DB was already seeded
             }
 
-            context.Clientes.AddRange(  //privados podem ter vários clientes
-                new Cliente { Nif = 999999999, Nome = "Sistema Central", Email = "jakimdascoives@upskill.pt" },
-                new Cliente { Nif = 123456789, Nome = "Joao", Email = "joao@upskill.pt" },
-                new Cliente { Nif = 987654321, Nome = "Jose", Email = "jose@upskill.pt" },
-                new Cliente { Nif = 112233445, Nome = "Raphael", Email = "raphael@upskill.pt" },
-                new Cliente { Nif = 998877665, Nome = "Ines", Email = "ines@upskill.pt" },
-                new Cliente { Nif = 554466887, Nome = "Daniel", Email = "daniel@upskill.pt" }
+            context.Clientes.AddRange(
+                new Cliente { Nif = 999999999, Nome = "Sistema Central", Email = "sistemacentraljakim@gmail.com" },
+                new Cliente { Nif = 222222222, Nome = "Fake Client 1", Email = "fakeclient1@upskill.pt" },
+                new Cliente { Nif = 333333333, Nome = "Fake Client 2", Email = "fakeclient2@upskill.pt" },
+                new Cliente { Nif = 444444444, Nome = "Fake Client 3", Email = "fakeclient3@upskill.pt" },
+                new Cliente { Nif = 555555555, Nome = "Fake Client 4", Email = "fakeclient4@upskill.pt" }
             );
             context.SaveChanges();
 
-            context.Freguesias.AddRange( //apenas uma freguesia
-                new Freguesia { Nome = "Currale de Moinas" },
-                new Freguesia { Nome = "Porto" }
-            );
+            context.Freguesias.Add(new Freguesia { Nome = "Porto" });
             context.SaveChanges();
 
-            context.Parques.AddRange(
-                new Parque { Rua = "Lugar das Moinas", FreguesiaId = 1 },
-                new Parque { Rua = "Lugar do Porto", FreguesiaId = 2 }
-            );
+            context.Parques.Add(new Parque { Rua = "Rua do parque privado 3", FreguesiaId = 1 });
             context.SaveChanges();
 
-            context.Lugares.AddRange(
-                new Lugar { Numero = 1, Fila = "AA", Andar = 1, ParqueId = 1, Preco = 5 },
-                new Lugar { Numero = 2, Fila = "BB", Andar = 2, ParqueId = 2, Preco = 6 },
-                new Lugar { Numero = 3, Fila = "CC", Andar = 3, ParqueId = 2, Preco = 7 },
-                new Lugar { Numero = 4, Fila = "DD", Andar = 4, ParqueId = 2, Preco = 8 },
-                new Lugar { Numero = 5, Fila = "EE", Andar = 5, ParqueId = 2, Preco = 9 }
-            );
+            context.Lugares.Add(new Lugar { Numero = 1, Fila = "A", Andar = -2, ParqueId = 1, Preco = 5.99 });
+            context.SaveChanges();
+            context.Lugares.Add(new Lugar { Numero = 2, Fila = "B", Andar = -1, ParqueId = 1, Preco = 4.99 });
+            context.SaveChanges();
+            context.Lugares.Add(new Lugar { Numero = 3, Fila = "C", Andar = 0, ParqueId = 1, Preco = 5.49 });
+            context.SaveChanges();
+            context.Lugares.Add(new Lugar { Numero = 4, Fila = "D", Andar = 1, ParqueId = 1, Preco = 4.89 });
+            context.SaveChanges();
+            context.Lugares.Add(new Lugar { Numero = 5, Fila = "E", Andar = 2, ParqueId = 1, Preco = 5.29 });
             context.SaveChanges();
 
-            context.Reservas.AddRange(
-                new Reserva { NifCliente = 123456789, LugarId = 1, Inicio = DateTime.Parse("2021-01-30 11:00:00"), Fim = DateTime.Parse("2021-01-30 12:00:00") },
-                new Reserva { NifCliente = 987654321, LugarId = 2, Inicio = DateTime.Parse("2021-01-29 21:00:00"), Fim = DateTime.Parse("2021-01-31 10:00:00") },
-                new Reserva { NifCliente = 554466887, LugarId = 3, Inicio = DateTime.Parse("2021-01-30 10:00:00"), Fim = DateTime.Parse("2021-01-30 13:00:00") },
-                new Reserva { NifCliente = 554466887, LugarId = 4, Inicio = DateTime.Parse("2021-01-30 11:00:00"), Fim = DateTime.Parse("2021-01-30 13:00:00") }
-            );
+            context.Reservas.Add(new Reserva { NifCliente = 222222222, LugarId = 1, Inicio = DateTime.Parse("2021-12-30 11:00:00"), Fim = DateTime.Parse("2021-12-30 12:00:00") });
+            context.SaveChanges();
+            context.Reservas.Add(new Reserva { NifCliente = 333333333, LugarId = 2, Inicio = DateTime.Parse("2021-12-29 21:00:00"), Fim = DateTime.Parse("2021-12-31 10:00:00") });
+            context.SaveChanges();
+            context.Reservas.Add(new Reserva { NifCliente = 444444444, LugarId = 3, Inicio = DateTime.Parse("2021-12-30 10:00:00"), Fim = DateTime.Parse("2021-12-30 13:00:00") });
+            context.SaveChanges();
+            context.Reservas.Add(new Reserva { NifCliente = 555555555, LugarId = 4, Inicio = DateTime.Parse("2021-12-30 11:00:00"), Fim = DateTime.Parse("2021-12-30 13:00:00") });
             context.SaveChanges();
         }
     }
