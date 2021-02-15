1d356e6eddf4601a079762822eb34ff63f754875
Jose Franco <jpmfranco@upskill.pt>
2021-02-15 18:41:56 +0000

    Testes SubAluguer sob Revisao

--- VSProjects/API-Sistema-Central/SCUnitTests/SubAlugueresControllerTest.cs
+++ VSProjects/API-Sistema-Central/SCUnitTests/SubAlugueresControllerTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -120,21 +120,21 @@ namespace SCUnitTests
         {
             //Arrange
             var mock = new Mock<ISubAluguerService>();
-            SubAlugueresController testController = new SubAlugueresController(mock.Object);
-            var model = new SubAluguerDTO
-            {
-                ParqueId = 1,
-                Numero = 10,
-                Fila = "A",
-                Andar = 0,
-                Preco = 5,
-                NifProprietario = "111111111",
-                ReservaOriginalId = 1,
-                Inicio = DateTime.Parse("2021-02-01 12:00:00"),
-                Fim = DateTime.Parse("2021-02-01 13:00:00"),
-            };
+            mock.Setup(s => s.PostSubAluguerAsync)
+                .ReturnsAsync(new SubAluguerDTO
+                {
+                    Id = 1,
+                    ParqueId = 1,
+                    Numero = 1,
+                    Fila = "A",
+                    Andar = 0,
+                    Preco = 4.99,
+                    NifProprietario = "999999999",
+                    Inicio = DateTime.Parse("2021-02-01T12:00:00"),
+                    Fim = DateTime.Parse("2021-02-01T13:00:00"),
+                });
             //Act
-            var result = await testController.PostSubAluguer(model);
+            var result = await controller.PostSubAluguer(model);
            // var get = await controller.GetSubAluguerById(10);
 
             //Assert
@@ -179,4 +179,360 @@ namespace SCUnitTests
         }
     }
 }
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using API_Sistema_Central.Models;
+using API_Sistema_Central.Controllers;
+using API_Sistema_Central.Services;
+using API_Sistema_Central.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
+namespace SCUnitTests
+{
+    [Collection("Testes SubAluguer")]
+    public class SubAlugueresControllerTest
+    {
+        ISubAluguerService serviceMocker;
+        public SubAlugueresControllerTest()
+        {
+
+            var SubAluguerList = new List<SubAluguerDTO>();
+            SubAluguerList.Add(new SubAluguerDTO()
+            {
+                Id = 1,
+                ParqueId = 1,
+                Numero = 1,
+                Fila = "A",
+                Andar = 0,
+                Preco = 4.99,
+                NifProprietario = "222222222",
+                Inicio = DateTime.Parse("2021-02-01T12:00:00"),
+                Fim = DateTime.Parse("2021-02-01T13:00:00"),
+            });
+            SubAluguerList.Add(new SubAluguerDTO()
+            {
+                Id = 2,
+                ParqueId = 1,
+                Numero = 1,
+                Fila = "A",
+                Andar = 0,
+                Preco = 4.99,
+                NifProprietario = "999999999",
+                Inicio = DateTime.Parse("2021-02-01T13:00:00"),
+                Fim = DateTime.Parse("2021-02-01T14:00:00"),
+            });
+            SubAluguerList.Add(new SubAluguerDTO()
+            {
+                Id = 3,
+                ParqueId = 2,
+                Numero = 1,
+                Fila = "A",
+                Andar = 0,
+                Preco = 4.99,
+                NifProprietario = "999999999",
+                Inicio = DateTime.Parse("2021-02-01T12:00:00"),
+                Fim = DateTime.Parse("2021-02-01T13:00:00"),
+            });
+            SubAluguerList.Add(new SubAluguerDTO()
+            {
+                Id = 4,
+                ParqueId = 2,
+                Numero = 2,
+                Fila = "A",
+                Andar = 0,
+                Preco = 4.99,
+                NifProprietario = "999999999",
+                Inicio = DateTime.Parse("2021-02-01T12:00:00"),
+                Fim = DateTime.Parse("2021-02-01T13:00:00"),
+            });
+
+
+            var mock = new Mock<ISubAluguerService>();
+
+           // mock.Setup(s => s.GetByNifAsync("999999999")).ReturnsAsync(new List<SubAluguerDTO>{
+
+            mock.Setup(x => x.GetByNifAsync("999999999")).ReturnsAsync(Task.FromResult(SubAluguerList[0]));
+
+            mock.Setup(x => x.DeleteAsync(It.Is<SubAluguerDTO>(c => string.Equals(c.CountryCode, "C1")))).Returns(Task.FromResult(true));
+
+            mock.Setup(x => x.UpdateAsync(It.Is<SubAluguerDTO>(c => string.Equals(c.CountryCode, "C1")))).Returns(Task.FromResult(true));
+            mock.Setup(x => x.UpdateAsync(It.Is<SubAluguerDTO>(c => string.Equals(c.CountryCode, "NoExCod")))).Returns(Task.FromResult(false));
+
+            serviceMocker = srv.Object;
+        }
+
+
+        [Fact]
+        public async Task GetAllCountriesAsync_ShouldReturnAllCountriesAsync()
+        {
+            // Arrange
+            var theController = new CountryController(theMockedService);
+
+            // Act
+            var result = await theController.GetCountries();
+
+            // Assert
+            var countries = Assert.IsType<List<Country>>(result.Value);
+            Assert.Equal(5, countries.Count());
+        }
+
+        [Fact]
+        public async Task GetCountryAsync_ShouldReturnNotFound()
+        {
+            // Arrange      
+            var theController = new CountryController(theMockedService);
+            var testCod = "C0";
+
+            // Act
+            var response = await theController.GetCountry(testCod);
+
+            //Assert       
+            Assert.IsType<NotFoundResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task GetCountryAsync_ShouldReturnCountry()
+        {
+            // Arrange       
+            var theController = new CountryController(theMockedService);
+            var testCod = "C1";
+
+            // Act
+            var result = await theController.GetCountry(testCod);
+
+            //Assert     
+            Assert.IsType<Country>(result.Value);
+        }
+
+        [Fact]
+        public async Task GetCountryAsync_ShouldReturnTheRightItemAsync()
+        {
+            //Arrange        
+            var theController = new CountryController(theMockedService);
+            var testCod = "C1";
+
+            //Act
+            var result = await theController.GetCountry(testCod);
+            var countryItem = result.Value;
+
+            //Assert     
+            Assert.IsType<Country>(countryItem);
+            Assert.Equal(testCod, (countryItem as Country).CountryCode);
+        }
+
+        [Fact]
+        public async Task PostBadNoNameCountryAsync_ShouldReturnBadRequest()
+        {
+            // Arrange     
+            var theController = new CountryController(theMockedService);
+            var noNameCountry = new Country
+            {
+                CountryCode = "XYZ",
+                GeoAreaCod = "Z1"
+            };
+            theController.ModelState.AddModelError("Name", "Required");
+
+            // Act
+            var response = await theController.PostCountry(noNameCountry);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task PostBadNoGeocodeCountryAsync_ShouldReturnBadRequest()
+        {
+            // Arrange   
+            var theController = new CountryController(theMockedService);
+            var noGeoAreaCountry = new Country
+            {
+                CountryCode = "XYZ",
+                Name = "Bad country"
+            };
+            theController.ModelState.AddModelError("GeoAreaCod", "Required");
+
+            // Act
+            var response = await theController.PostCountry(noGeoAreaCountry);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task PostValidCountryAsync_ShouldReturnCreatedResponse()
+        {
+            // Arrange     
+            var theController = new CountryController(theMockedService);
+            var theNewCountry = new Country
+            {
+                CountryCode = "NEW",
+                Name = "The New Country",
+                GeoAreaCod = "Z1"
+            };
+
+            // Act
+            var response = await theController.PostCountry(theNewCountry);
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task PostCountryAsync_ShouldCreateAnCountryAsync()
+        {
+            // Arrange       
+            var theController = new CountryController(theMockedService);
+            var theNewCountry = new Country
+            {
+                CountryCode = "OTHER",
+                Name = "Another New Country",
+                GeoAreaCod = "Z3"
+            };
+
+            // Act
+            var response = await theController.PostCountry(theNewCountry);
+            var result = response.Result as CreatedAtActionResult;
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.IsNotType<BadRequestObjectResult>(result);
+            Assert.IsType<Country>(result.Value);
+
+            var theCountry = result.Value as Country;
+            Assert.Equal("OTHER", theCountry.CountryCode);
+        }
+
+        [Fact]
+        public async Task PutNoExistingCountryAsync_ShouldReturnNotFound()
+        {
+            // Arrange      
+            var theController = new CountryController(theMockedService);
+            var testCod = "NoExCod";
+            var theNonExistingCountry = new Country
+            {
+                CountryCode = testCod,
+                Name = "Updated country " + testCod,
+                GeoAreaCod = "Z1"
+            };
+
+            // Act
+            var response = await theController.PutCountry(testCod, theNonExistingCountry);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+        [Fact]
+        public async Task PutBadNoNameCountryAsync_ShouldReturnBadRequest()
+        {
+            // Arrange      
+            var theController = new CountryController(theMockedService);
+
+            var testCod = "C1";
+            var noNameCountry = new Country
+            {
+                CountryCode = testCod,
+                GeoAreaCod = "Z1"
+            };
+
+
+            theController.ModelState.AddModelError("Name", "Required");
+
+            // Act
+            var response = await theController.PutCountry(testCod, noNameCountry);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public async Task PutBadNoGeoAreaCountryAsync_ShouldReturnBadRequest()
+        {
+            // Arrange        
+            var theController = new CountryController(theMockedService);
+
+            var testCod = "C1";
+            var noNameCountry = new Country
+            {
+                CountryCode = testCod,
+                Name = "Updated Name"
+            };
+
+            theController.ModelState.AddModelError("GeoAreaCod", "Required");
+
+            // Act
+            var response = await theController.PutCountry(testCod, noNameCountry);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public async Task PutCountry_ShouldReturnCreatedResponse()
+        {
+            // Arrange     
+            var theController = new CountryController(theMockedService);
+            var testCod = "C1";
+            var theCountry = new Country
+            {
+                CountryCode = testCod,
+                Name = "Updated Country",
+                GeoAreaCod = "Z1"
+            };
+
+            // Act
+            var response = await theController.PutCountry(testCod, theCountry);
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(response);
+        }
+
+        [Fact]
+        public async Task DeleteNotExistingCountry_ShouldReturnNotFound()
+        {
+            // Arrange       
+            var theController = new CountryController(theMockedService);
+            var testCod = "NoExCod";
+
+            // Act
+            var result = await theController.DeleteCountry(testCod);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteExistingCountryAsync_ShouldReturnOkResult()
+        {
+            // Arrange       
+            var theController = new CountryController(theMockedService);
+            var testCod = "C1";
+
+            // Act
+            var response = await theController.DeleteCountry(testCod);
+
+            // Assert
+            Assert.IsType<NoContentResult>(response);
+        }
+
+        [Fact]
+        public async Task DeleteExistingCountryAsync_ShouldRemovetheCountryAsync()
+        {
+            // Arrange       
+            var theController = new CountryController(theMockedService);
+            var testCod = "C1";
+
+            // Act
+            var result = await theController.DeleteCountry(testCod);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+    }
+}
