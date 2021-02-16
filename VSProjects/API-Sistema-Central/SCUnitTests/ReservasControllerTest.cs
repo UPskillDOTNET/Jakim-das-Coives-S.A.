e9aaa486706408c3fdca7e7310293ee230f5a6ea
Daniel Macedo <dcmacedo02@upskill.pt>
2021-02-16 19:40:28 +0000

    ReservasDisponiveisTest Funcional

--- VSProjects/API-Sistema-Central/SCUnitTests/ReservasControllerTest.cs
+++ VSProjects/API-Sistema-Central/SCUnitTests/ReservasControllerTest.cs
@@ -137,34 +137,36 @@ namespace SCUnitTests
             Assert.Equal("randomLink", theNewReserva.ApiUrl);
         }
 
-        /*[Fact]
+        [Fact]
         public async void GetDisponilibidade_ShouldReturnLugaresDisponiveisNaFreguesiaDataHora()
         {
             //Arrange
             var mock = new Mock<IReservaService>();
-            mock.Setup(p => p.FindAvailableAsync("Porto", DateTime.Parse("2021-01-30 11:00:00"), DateTime.Parse("2021-01-30 13:00:00"))
-                .ReturnsAsync(new LugarDTO
+            mock.Setup(p => p.FindAvailableAsync("Porto", DateTime.Parse("2021-01-30 11:00:00"), DateTime.Parse("2021-01-30 13:00:00")))
+                .ReturnsAsync(new List<LugarDTO>
                 {
-                    Id = 1,
-                    ParqueId = 1,
-                    Numero = 1,
-                    Fila = "A",
-                    Andar = 1,
-                    Preco = 10,
-                    NifProprietario = "999999999",
-                    ReservaOriginalId = 1,
-                    ParqueIdSC = 1,
-                    ApiUrl = "randomlink"
+                    new LugarDTO {
+                        Id = 1,
+                        ParqueId = 1,
+                        Numero = 1,
+                        Fila = "A",
+                        Andar = 1,
+                        Preco = 10,
+                        NifProprietario = "999999999",
+                        ReservaOriginalId = 1,
+                        ParqueIdSC = 1,
+                        ApiUrl = "randomlink"
+                    }
                 });
 
                 ReservasController testController = new ReservasController(mock.Object);
 
                 var result = await testController.FindAvailableAsync("Porto", DateTime.Parse("2021-01-30 11:00:00"), DateTime.Parse("2021-01-30 13:00:00"));
 
-                var item = Assert.IsType<Reserva>(result.Value);
-                Assert.Equal("999999999", item.NifUtilizador);
-                Assert.Equal(1, item.ParqueId);
-        }*/
+                var item = Assert.IsType<List<LugarDTO>>(result.Value);
+                Assert.Equal(1, item[0].Id);
+                Assert.Equal("A", item[0].Fila);
+        }
 
         private static T GetObjectResultContent<T>(ActionResult<T> result)
         {
