7dc6ad92539d85878b663cff1041ddccb77a670f
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-02-26 19:10:21 +0000

    Apagar ErrorViewModel e todos os seus traços

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Controllers/HomeController.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Controllers/HomeController.cs
@@ -30,11 +30,5 @@ namespace APP_FrontEnd.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
