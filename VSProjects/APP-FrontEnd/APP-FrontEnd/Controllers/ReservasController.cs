a1de08aa4d1ace8f15008dc358b1a0eba96832d0
Daniel Macedo <dcmacedo02@upskill.pt>
2021-03-01 15:20:59 +0000

    ReservasController Simples

---
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Controllers/ReservasController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using APP_FrontEnd.Models;
+using APP_FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP_FrontEnd.Controllers
+{
+    public class ReservasController : Controller
+    {
+        private readonly IReservaService _reservaService;
+
+        public ReservasController(IReservaService reservaService)
+        {
+            _reservaService = reservaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var lista = await _reservaService.GetByNifAsync();
+                return View(lista);
+            }
+            catch (Exception e)
+            {
+                return MensagemErro(e.Message);
+            }
+        }
+
+        private IActionResult MensagemErro(string mensagem)
+        {
+            return View("MensagemErro", new MensagemErro { Mensagem = mensagem });
+        }
+    }
+}
