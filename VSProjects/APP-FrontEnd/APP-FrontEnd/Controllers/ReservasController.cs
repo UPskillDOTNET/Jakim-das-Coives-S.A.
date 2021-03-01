093b639124bba4a7e5cb609053c19e0239a703d9
Daniel Macedo <dcmacedo02@upskill.pt>
2021-03-01 15:36:34 +0000

    Metodo Post Reservar

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Controllers/ReservasController.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Controllers/ReservasController.cs
@@ -28,6 +28,44 @@ namespace APP_FrontEnd.Controllers
             }
         }
 
+        public async Task<IActionResult> VerReservasDisponiveis(string freguesiaNome, DateTime inicio, DateTime fim)
+        {
+            try
+            {
+                var lista = await _reservaService.FindAvailableAsync(freguesiaNome, inicio, fim);
+                return View(lista);
+            }
+            catch (Exception e)
+            {
+                return MensagemErro(e.Message);
+            }
+        }
+
+        public IActionResult Reservar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Reservar(ReservaDTO reservaDTO)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _reservaService.PostAsync(reservaDTO);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception e)
+                {
+                    return MensagemErro(e.Message);
+                }
+            }
+            return View(reservaDTO);
+        }
+
         private IActionResult MensagemErro(string mensagem)
         {
             return View("MensagemErro", new MensagemErro { Mensagem = mensagem });
