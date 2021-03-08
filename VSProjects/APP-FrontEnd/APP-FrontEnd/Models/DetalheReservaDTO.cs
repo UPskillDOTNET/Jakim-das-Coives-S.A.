48a4270d4e66da1eb32c3472bea83158ef8aba2d
Ines Oliveira <ines.machado.oliveira@upskill.pt>
2021-03-08 15:19:19 +0000

    Estilizar Reservas Index

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Models/DetalheReservaDTO.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Models/DetalheReservaDTO.cs
@@ -10,8 +10,12 @@ namespace APP_FrontEnd.Models
     {
         public int ReservaId { get; set; }
         public double Custo { get; set; }
+
+        [Display(Name = "Nº da Reserva")]
         public int ReservaParqueId { get; set; }
         public string NomeFreguesia { get; set; }
+
+        [Display(Name = "Parque")]
         public string NomeParque { get; set; }
         public int NumeroLugar { get; set; }
         public string Fila { get; set; }
@@ -20,9 +24,11 @@ namespace APP_FrontEnd.Models
         [RegularExpression(@"^\d{9}$", ErrorMessage = "NIF inválido")]
         public string NifProprietario { get; set; }
         [DataType(DataType.DateTime)]
+        [Display(Name = "Data de Início")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime Inicio { get; set; }
         [DataType(DataType.DateTime)]
+        [Display(Name = "Data de Fim")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime Fim { get; set; }
     }
