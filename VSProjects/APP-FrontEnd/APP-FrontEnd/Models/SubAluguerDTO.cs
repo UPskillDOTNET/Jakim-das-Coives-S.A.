53af0969485489d9109d40cf9d4a677766910221
Ines Oliveira <ines.machado.oliveira@upskill.pt>
2021-03-08 16:26:19 +0000

    Estilizar SubAluguer Index

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Models/SubAluguerDTO.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Models/SubAluguerDTO.cs
@@ -8,17 +8,24 @@ namespace APP_FrontEnd.Models
 {
     public class SubAluguerDTO
     {
+        [Display(Name = "Nº de Sub-Aluguer")]
         public int Id { get; set; }
         public int ParqueId { get; set; }
+
+        [Display(Name = "Parque")]
         public string NomeParque { get; set; }
         public int Numero { get; set; }
         public string Fila { get; set; }
         public int Andar { get; set; }
         [DataType(DataType.Currency)]
+
+        [Display(Name = "Preço")]
         public double Preco { get; set; }
         [RegularExpression(@"^\d{9}$", ErrorMessage = "NIF inválido")]
         public string NifProprietario { get; set; }
         public int ReservaSistemaCentralId { get; set; }
+
+        [Display(Name = "Encontra-se Reservado?")]
         public bool? IsReservado { get; set; }
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
