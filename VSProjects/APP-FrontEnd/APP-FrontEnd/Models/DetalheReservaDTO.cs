bbe8c3fc689ade7ef0214d9d0ccb61647ea76e0d
Ines Oliveira <ines.machado.oliveira@upskill.pt>
2021-03-09 12:19:06 +0000

    Estilizando Reservas/Detalhes

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Models/DetalheReservaDTO.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Models/DetalheReservaDTO.cs
@@ -10,17 +10,25 @@ namespace APP_FrontEnd.Models
     {
         public int ReservaId { get; set; }
         [DataType(DataType.Currency)]
+
+        [Display(Name = "Preço")]
         public double Custo { get; set; }
 
         [Display(Name = "Nº da Reserva")]
         public int ReservaParqueId { get; set; }
+
+        [Display(Name = "Freguesia")]
         public string NomeFreguesia { get; set; }
 
         [Display(Name = "Parque")]
         public string NomeParque { get; set; }
+
+        [Display(Name = "Nº de Lugar")]
         public int NumeroLugar { get; set; }
         public string Fila { get; set; }
         public int Andar { get; set; }
+
+        [Display(Name = "Encontra-se Sub-Alugado?")]
         public bool IsSubAlugado { get; set; }
         [RegularExpression(@"^\d{9}$", ErrorMessage = "NIF inválido")]
         public string NifProprietario { get; set; }
