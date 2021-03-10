c68c2b89aea287ce608b9cebafa4372b5da87058
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2021-03-10 19:26:15 +0000

    Logout - rescindir refreshToken e apagar cookie

--- VSProjects/APP-FrontEnd/APP-FrontEnd/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ VSProjects/APP-FrontEnd/APP-FrontEnd/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using APP_FrontEnd.Services;
+using System.Net.Http.Headers;
 
 namespace APP_FrontEnd.Areas.Identity.Pages.Account
 {
@@ -19,11 +23,15 @@ namespace APP_FrontEnd.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<Utilizador> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ITokenService _tokenService;
 
-        public LogoutModel(SignInManager<Utilizador> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<Utilizador> signInManager, ILogger<LogoutModel> logger, IHttpContextAccessor httpContextAccessor, ITokenService tokenService)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+            _tokenService = tokenService;
         }
 
         public void OnGet()
@@ -32,9 +40,9 @@ namespace APP_FrontEnd.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
-            await _signInManager.SignOutAsync();
+            await RevokeTokenAsync();
 
-            //await RevokeTokenAsync(token);
+            await _signInManager.SignOutAsync();
 
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
@@ -47,21 +55,41 @@ namespace APP_FrontEnd.Areas.Identity.Pages.Account
             }
         }
 
-        private async Task RevokeTokenAsync(TokenResponse token)
+        private async Task RevokeTokenAsync()
         {
+            string token = "";
             try
             {
-                using (HttpClient client = new HttpClient())
-                {
-                    StringContent content = new StringContent(JsonConvert.SerializeObject(token), Encoding.UTF8, "application/json");
-                    string endpoint = "https://localhost:5050/api/utilizadores/rescindir-token";
-                    var response = await client.PostAsync(endpoint, content);
-                    response.EnsureSuccessStatusCode();
-                }
+                token = await _tokenService.GetTokenAsync();
             }
             catch
             {
-                throw new Exception("Rescindir refresh token falhou no servidor. Volte a tentar.");
+            }
+
+            if (token != "")
+            {
+                try
+                {
+                    var refreshToken = _httpContextAccessor.HttpContext.Request.Cookies["refreshToken"];
+                    var cookie = new Cookie { Name = "refreshToken", Value = refreshToken, Path = "/", Domain = "localhost", Expires = DateTime.UtcNow.AddMinutes(1), HttpOnly = true };
+                    var tokenResponse = new TokenResponse();
+                    var cookieContainer = new CookieContainer();
+                    cookieContainer.Add(cookie);
+                    using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                        StringContent content = new StringContent(JsonConvert.SerializeObject(new RevokeTokenRequest { Token = refreshToken }), Encoding.UTF8, "application/json");
+                        string endpoint = "https://localhost:5050/api/utilizadores/rescindir-token";
+                        var response = await client.PostAsync(endpoint, content);
+                        response.EnsureSuccessStatusCode();
+                    }
+                    Response.Cookies.Delete("refreshToken");
+                }
+                catch
+                {
+                    throw new Exception("Rescindir refresh token falhou no servidor. Volte a tentar.");
+                }
             }
         }
     }
